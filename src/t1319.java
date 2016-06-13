1dfc4d3be635784ea02fcad10e057e8227a0e786
DSamokhina <prishliemail@gmail.com>
2016-06-13 14:18:52 +0300

    solved t1319

--- src/t1319.java
+++ src/t1319.java
@@ -6,29 +6,43 @@ public class t1319 {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int n = in.nextInt();
-        int[] mas = new int[n];
-        int dop=1, t=1;
-        ArrayList<int[]> allArrays = new ArrayList<int[]>();
-
-
-        for (int j=1;j<=n;j++) {
-
-            if (j>1){
-                t = mas[n-2]+1;
+        int[][] mas = new int[n][n];
+        int k = (n+1)*n/2;
+        int a=0, b=0;
+        for (int i=1;i<=k;i++) {
+            mas[a][b] = i;
+            if (b==0){
+                b=a+1;
+                a=0;
             }
-            int p=j-1;
-            for (int i = dop; i < n+dop; i++) {
-                if (i>dop) {
-                    t = t+p;
-                }
-                mas[n + dop - i-1] = t;
-                p=p+1;
-
+            else{
+                a=a+1;
+                b=b-1;
             }
-            dop=dop+1;
-            allArrays.add(mas);
-
         }
+
+        a=n-1;
+        b=n-1;
+        for (int i=n*n;i>k;i--) {
+            mas[a][b] = i;
+            if (b==n-1){
+                b=a-1;
+                a=n-1;
+            }
+            else{
+                a=a-1;
+                b=b+1;
+            }
+        }
+
+
+        for (int l=0; l<n; l++){
+            for(int m=n-1; m>=0; m--){
+                out.print(mas[l][m] + " ");
+            }
+            out.println("");
+        }
+
         out.flush();
     }
 }
